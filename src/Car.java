2da10491aa8044b9ab5a181225383c79a5346008
Jamie Brooks <jbrooksi@yahoo.com>
2016-08-29 10:10:36 -0400

    New New

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,52 @@
 
 public class Car {
 
+	String make;
+	String Model;
+	int year;
+	double price;
+	
+	public Car(String make, String model, int year, double price){
+	this.make = make;
+	this.Model = model;
+	this.price = price;
+	this.year = year;
+	
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return Model;
+	}
+
+	public void setModel(String model) {
+		Model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+public String toString(){
+	return (getMake() + getModel() + getYear() + getPrice());
+}
 }
--- src/UsedCar.java
+++ src/UsedCar.java
@@ -1,4 +1,23 @@
 
-public class UsedCar {
+public class UsedCar extends Car {
+	int miles;
 
+	public UsedCar(String make, String model, int year, double price, int Miles ){
+	
+	super(make, model, year, price);
+	this.miles= miles;
+	
+}
+
+	public int getMiles() {
+		return miles;
+	}
+
+	public void setMiles(int additionalMiles) {
+		this.miles = additionalMiles;
+	}
+
+	public String toString(){
+		return (getMake() + getModel() + getYear() + getPrice() + getMiles() + "Miles");
+	}
 }
